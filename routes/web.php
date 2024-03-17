e2d7cede17abde64ae0926846b6ce999f87a3fd7
Gabriel Moskwa <gabriel.moskwa@wp.pl>
2024-03-17 22:03:55 +0100

    swagger + register/login

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/test', function () {
+    return 'test1';
+});
+Route::prefix('api')->group(function () {
+    Route::post('/register', [UserController::class, 'register']);
+    Route::post('/login', 'App\Http\Controllers\LoginController@login');
+    Route::get('/test', 'App\Http\Controllers\UserController@getTest');
+
+    Route::get('/user', 'App\Http\Controllers\UserController@getUserDetails')->middleware('auth:sanctum');
 });
